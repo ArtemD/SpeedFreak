e5596c8e8492c120bb15c404d3e0142061ad09ab
Olavi Pulkkinen <olavi.pulkkinen@fudeco.com>
2010-03-17 10:25:31 +0200

    Addede route dialog to view stored route.

---
+++ Client/routedialog.h
@@ -0,0 +1,26 @@
+#ifndef ROUTEDIALOG_H
+#define ROUTEDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+    class RouteDialog;
+}
+
+class RouteDialog : public QDialog {
+    Q_OBJECT
+public:
+    RouteDialog(QWidget *parent = 0);
+    ~RouteDialog();
+
+protected:
+    void changeEvent(QEvent *e);
+
+private:
+    Ui::RouteDialog *ui;
+
+private slots:
+    void on_closePushButton_clicked();
+};
+
+#endif // ROUTEDIALOG_H
