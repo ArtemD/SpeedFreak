4eca45a042f57004c559603274b95d5e675acc90
Janne Änäkkälä <janne.anakkala@fudeco.com>
2010-03-30 15:45:26 +0300

    Bug fixed in result dialog.

--- Client/resultdialog.cpp
+++ Client/resultdialog.cpp
@@ -60,6 +60,8 @@ ResultDialog::ResultDialog(QWidget *parent) :
     speedList << "0" << "10" << "20" << "30" << "40" << "50" << "60" << "70" << "80" << "90" << "100" ;
     timeList << "0" << "1" << "2" << "3" << "4" << "5" << "6" << "7" << "8" << "9" << "10" << "11"
             << "12" << "13" << "14" << "15" << "16" << "17" << "18" << "19" << "20";
+    this->diagramGapStem = DIAGRAMGAP100KMH;
+    this->diagramGapHorizontal = DIAGRAMGAP20S;
     for (int i = 0; i < 11; i++)
     {
         timeArray[i] = 0;
@@ -293,6 +295,7 @@ void ResultDialog::on_pushButtonNew_clicked()
     {
         timeArray[i] = 0;
     }
+    resultString = "";
     this->close();
 }
 
