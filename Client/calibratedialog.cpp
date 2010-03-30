7a8ae641c46ec97377e0b67c0fb81f87d9b397eb
Kai Rasilainen <kai.rasilainen@gmail.com>
2010-03-30 14:36:07 +0300

    Calibrate dialog addition.

---
+++ Client/calibratedialog.cpp
@@ -0,0 +1,45 @@
+/*
+ * Calibrate dialog
+ *
+ * @author     Kai Rasilainen <kai.rasilainen@fudeco.com>
+ * @copyright  (c) 2010 Speed Freak team
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+#include "calibratedialog.h"
+#include "ui_calibratedialog.h"
+
+CalibrateDialog::CalibrateDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::CalibrateDialog)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("Calibrating...");
+}
+
+CalibrateDialog::~CalibrateDialog()
+{
+    delete ui;
+}
+
+void CalibrateDialog::changeEvent(QEvent *e)
+{
+    QDialog::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        ui->retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+}
+
+void CalibrateDialog::setProgressValue(int value)
+{
+    ui->progressBar->setValue(value);
+}
+
+void CalibrateDialog::resetProgressValue()
+{
+    ui->progressBar->reset();
+}
---
+++ Client/calibratedialog.h
@@ -0,0 +1,34 @@
+/*
+ * Calibrate dialog
+ *
+ * @author     Kai Rasilainen <kai.rasilainen@fudeco.com>
+ * @copyright  (c) 2010 Speed Freak team
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+#ifndef CALIBRATEDIALOG_H
+#define CALIBRATEDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+    class CalibrateDialog;
+}
+
+class CalibrateDialog : public QDialog {
+    Q_OBJECT
+public:
+    CalibrateDialog(QWidget *parent = 0);
+    ~CalibrateDialog();
+
+    void setProgressValue(int value);
+    void resetProgressValue();
+
+protected:
+    void changeEvent(QEvent *e);
+
+private:
+    Ui::CalibrateDialog *ui;
+};
+
+#endif // CALIBRATEDIALOG_H
