c683afd542a2de84235f67e44e30fbaa5c6ee9a2
Artem Daniliants <artem@daniliants.com>
2010-03-31 13:28:18 +0300

    Added template

--- Server/application/views/pages/base.php
+++ Server/application/views/pages/base.php
@@ -1,15 +1,197 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title></title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="<?php echo url::base(); ?>static/css/style.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo url::base(); ?>static/css/layout.css" rel="stylesheet" type="text/css" />
+<script src="<?php echo url::base(); ?>static/js/jquery-1.3.2.min.js" type="text/javascript"></script>
+<script src="<?php echo url::base(); ?>static/js/cufon-yui.js" type="text/javascript"></script>
+<script src="<?php echo url::base(); ?>static/js/cufon-replace.js" type="text/javascript"></script>
+<script src="<?php echo url::base(); ?>static/js/Myriad_Pro_700.font.js" type="text/javascript"></script>
+<script src="<?php echo url::base(); ?>static/js/Myriad_Pro_BC_700.font.js" type="text/javascript"></script>
 <!-- Google analytics -->
-	<script type="text/javascript">
-	
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-13272012-1']);
-	  _gaq.push(['_trackPageview']);
-	
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	
-	</script>
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-13272012-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
 <!-- /Google analytics -->
+</head>
+
+<body id="page5">
+    <div class="top-bg">
+    <div id="main">
+        <!--header -->
+        <div id="header">
+            <div class="menu">
+                <div class="left-bg">
+                    <div class="right-bg">
+                        <a href="index.html" class="home-link"><img src="<?php echo url::base(); ?>static/img/home-link.gif" alt="" /></a>
+                        <ul>
+                            <li><a href="index.html">About</a></li>
+                            <li><a href="index-1.html">Services</a></li>
+                            <li><a href="index-2.html">Gallery</a></li>
+                            <li><a href="index-3.html">Events</a></li>
+                            <li><a href="index-4.html" class="active">Contacts</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="logo"><a href="index.html"><img src="<?php echo url::base(); ?>static/img/logo.jpg" alt="" /></a></div>
+        </div>
+        <!--header end-->
+        <div id="content">
+            <div class="box1">
+                <div class="left-border">
+                    <div class="right-border">
+                        <div class="top-border">
+                            <div class="left-bot">
+                                <div class="right-bot">
+                                    <div class="left-top">
+                                        <div class="right-top">
+                                            <div class="indent4 line2">
+                                                <div class="container">
+                                                    <div class="col-1">
+                                                        <h4>Our locations</h4>
+                                                        <strong>United States of America </strong>
+                                                        Fusce euismod consequat ante. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque sed dolor. Aliquam congue fermentum nisl. Mauris accumsan nulla vel diam.
+                                                        <p>
+                                                            <strong>Netherlands</strong>
+                                                            Sed in lacus ut enim adipiscing aliquet. Nulla venenatis. In pede mi, aliquet sit amet, euism in, auctor ut, ligula. Aliquam dapibus tincidunt metus. Praesent justo dolor, lobortis quis, lobortis dignissim, pulvinar ac, lorem. Vestibulum sed ante.
+                                                        </p>
+                                                        <p>
+                                                            <strong>New Zealand</strong>
+                                                            Maecenas tristique orci ac sem. Duis ultricies pharetra magna. Donec accumsan malesuada<br /> orci. Donec sit amet eros. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                                                        </p>
+                                                    </div>
+                                                    <div class="col-2 space1">
+                                                        <h4>Useful links</h4>
+                                                        <ul class="list">
+                                                            <li><a href="#">Lorem ipsum dolor vestibulum </a></li>
+                                                            <li><a href="#">Aenean nonummy hendrerit </a></li>
+                                                            <li><a href="#">Vivamus eget nibh Etiam cursus </a></li>
+                                                            <li><a href="#">Nulla facilisi Aenean nec eros </a></li>
+                                                            <li><a href="#">Vestibulum ante ipsum primis </a></li>
+                                                            <li><a href="#">In faucibus orci luctus et ltrices pos </a></li>
+                                                            <li><a href="#">Suspendisse sollicitudin velit leo </a></li>
+                                                            <li><a href="#">Ut pharetra augue nec augue Nam </a></li>
+                                                            <li><a href="#">Tincidunt ac, viverra sed, nulla onec </a></li>
+                                                            <li class="last"><a href="#">Quisque diam lorem Interdum </a></li>
+                                                        </ul>
+                                                        <div class="container"><p><a href="#" class="link"><span><span>More Info</span></span></a></p></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="indent">
+                <div class="container">
+                    <div class="col-1">
+                        <div class="box2">
+                            <div class="left-border">
+                                <div class="right-border">
+                                    <div class="bot-border">
+                                        <div class="left-top">
+                                            <div class="right-top">
+                                                <div class="left-bot">
+                                                    <div class="right-bot">
+                                                        <div class="title">
+                                                            <div class="left-bg">
+                                                                <div class="right-bg">
+                                                                    <h5>Postal address</h5>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="indent1 text4">
+                                                            Pellentesque sed dolor. Aliquam congue fermentum nisl. Mauris accumsan nulla vel diam. Sed in lacus ut enim adipiscing aliquet. Nulla venenatis.
+                                                            <div class="padding2">
+                                                                <p class="text5">
+                                                                    <strong>The Company Name Inc.<br />
+                                                                    8901 Marmora Road,<br />
+                                                                    Glasgow, D04 89GR.</strong>
+                                                                </p>
+                                                                <p class="padding1 text5">
+                                                                    <strong><span class="phone2">+1 800 559 6580</span>Freephone:<br />
+                                                                    <span class="phone2">+1 959 603 6035</span>Telephone:<br />
+                                                                    <span class="phone2">+1 504 889 9898</span>FAX:<br />
+                                                                    E-mail: <a href="#">mail@companyname.com</a></strong>
+                                                                </p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-2 space2">
+                        <div class="box2">
+                            <div class="left-border">
+                                <div class="right-border">
+                                    <div class="bot-border">
+                                        <div class="left-top">
+                                            <div class="right-top">
+                                                <div class="left-bot">
+                                                    <div class="right-bot">
+                                                        <div class="title">
+                                                            <div class="left-bg">
+                                                                <div class="right-bg">
+                                                                    <h5>Contact form</h5>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="indent1">
+                                                            <form action="" method="get" enctype="multipart/form-data" id="ContactForm">
+                                                                <div>
+                                                                    <label><input type="text" name="name" onblur="if(this.value=='') this.value='Your Name:'" onfocus="if(this.value =='Your Name:' ) this.value=''" value="Your Name:" /></label>
+                                                                    <label><input type="text" name="phone" onblur="if(this.value=='') this.value='Telephone:'" onfocus="if(this.value =='Telephone:' ) this.value=''" value="Telephone:" /></label>
+                                                                    <label><input type="text" name="email" onblur="if(this.value=='') this.value='E-mail:'" onfocus="if(this.value =='E-mail:' ) this.value=''" value="E-mail:" /></label>
+                                                                    <textarea cols="10" rows="3" onblur="if(this.value=='') this.value='Message:'" onfocus="if(this.value =='Message:' ) this.value=''" name="msg">Message:</textarea>
+                                                                </div>
+                                                                <div class="container text2">
+                                                                    <a href="#" class="link2" onclick="document.getElementById('ContactForm').submit()"><span><span>Send</span></span></a>
+                                                                    <a href="#" class="link2" onclick="document.getElementById('ContactForm').reset()"><span><span>Clear</span></span></a>
+                                                                </div>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--footer -->
+        <div id="footer">
+            <p>Car tuning &copy; 2009    <a href="index-5.html">Privacy Policy</a></p>
+        </div>
+        <!--footer end-->
+    </div>
+    </div>
+    <script type="text/javascript"> Cufon.now(); </script>
+</body>
+
+</html>
