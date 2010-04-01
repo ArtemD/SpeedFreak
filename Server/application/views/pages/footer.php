82b9c0458f12324a0ac931161ce64d16e1e93877
Artem Daniliants <artem@daniliants.com>
2010-04-01 06:19:05 +0300

    Added attribution for icon

--- Server/application/views/pages/footer.php
+++ Server/application/views/pages/footer.php
@@ -1,5 +1,5 @@
 		<div id="footer"> 
-			<p><a href="<?php echo url::base(); ?>" title="SpeedFreak's homepage">SpeedFreak-app.com</a> &copy; 2010</p> 
+			<p><a href="<?php echo url::base(); ?>" title="SpeedFreak's homepage">SpeedFreak-app.com</a> &copy; 2010 | Icon by <a href="http://barrymieny.deviantart.com/">barrymieny</a></p>
 		</div>
 	</div> 
 	</div> 
