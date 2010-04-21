3057d1d8cca8fa8db75d11b5fccf00889a9da3d5
Artem Daniliants <artem@daniliants.com>
2010-04-21 11:07:31 +0300

    Fixed bug when submitting results that have decimals

--- Server/application/models/database_sample_data.sql
+++ Server/application/models/database_sample_data.sql
@@ -1,57 +1,52 @@
--- MySQL dump 10.11
---
--- Host: localhost    Database: speedfreak
--- ------------------------------------------------------
--- Server version	5.0.51a-24+lenny3-log
+﻿# Sequel Pro dump
+# Version 1630
+# http://code.google.com/p/sequel-pro
+#
+# Host: localhost (MySQL 5.1.37)
+# Database: speedfreak
+# Generation Time: 2010-04-21 11:03:21 +0300
+# ************************************************************
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Dumping data for table `categories`
---
+
+# Dump of table categories
+# ------------------------------------------------------------
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,'acceleration-0-20','Acceleration from 0km/h to 20km/h','km/h'),(2,'acceleration-0-40','Acceleration from 0km/h to 40km/h','km/h'),(3,'acceleration-0-100','Acceleration from 0km/h to 100km/h','km/h');
+INSERT INTO `categories` (`id`,`slug`,`description`,`unit`)
+VALUES
+	(1,'acceleration-0-10','Acceleration from 0km/h to 100km/h','km/h'),
+	(2,'acceleration-0-40','Acceleration from 0km/h to 100km/h','km/h'),
+	(3,'acceleration-0-100','Acceleration from 0km/h to 100km/h','km/h');
+
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Dumping data for table `results`
---
 
-LOCK TABLES `results` WRITE;
-/*!40000 ALTER TABLE `results` DISABLE KEYS */;
-INSERT INTO `results` VALUES (3,4,2,'2010-04-01 04:55:39'),(3,5,3,'2010-04-01 04:55:46'),(3,6,5,'2010-04-01 04:55:55'),(3,7,12,'2010-04-01 04:56:01'),(3,8,19,'2010-04-01 04:56:07'),(3,9,21,'2010-04-01 04:56:14'),(3,10,24,'2010-04-01 04:56:21'),(3,11,30,'2010-04-01 04:56:29'),(3,12,33,'2010-04-01 04:56:33'),(3,12,35,'2010-04-01 04:56:36'),(3,13,35,'2010-04-01 04:56:40'),(3,14,46,'2010-04-01 04:56:47');
-/*!40000 ALTER TABLE `results` ENABLE KEYS */;
-UNLOCK TABLES;
+# Dump of table results
+# ------------------------------------------------------------
 
---
--- Dumping data for table `users`
---
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (4,'Seppo','dfs','seppo@seppo.com'),(5,'Artem','dfs','seppzo@seppo.com'),(6,'Jukka','dfs','seppzzo@seppo.com'),(7,'Kai','dfs','seppzzzo@seppo.com'),(8,'Obama','dfs','seppzzszo@seppo.com'),(9,'Halonen','dfs','seppzszszo@seppo.com'),(10,'Pertti','dfs','seppzszzszo@seppo.com'),(11,'Temchu','dfs','seppzszzszo@sezppo.com'),(12,'Random','dfs','seppzszzszo@zsezppo.com'),(13,'Martin','dfs','szeppzszzszo@zsezppo.com'),(14,'Johnny','dfs','szeppzszzszo@zsezppzo.com');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+# Dump of table users
+# ------------------------------------------------------------
+
+
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2010-04-01  2:22:27
--- Server/application/models/database_structure.sql
+++ Server/application/models/database_structure.sql
@@ -1,81 +1,80 @@
--- MySQL dump 10.11
---
--- Host: localhost    Database: speedfreak
--- ------------------------------------------------------
--- Server version	5.0.51a-24+lenny3-log
+﻿# Sequel Pro dump
+# Version 1630
+# http://code.google.com/p/sequel-pro
+#
+# Host: localhost (MySQL 5.1.37)
+# Database: speedfreak
+# Generation Time: 2010-04-21 11:03:11 +0300
+# ************************************************************
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `categories`
---
+
+# Dump of table categories
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `categories`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+
 CREATE TABLE `categories` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `slug` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `unit` varchar(255) NOT NULL,
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY (`id`),
   KEY `slug` (`slug`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-SET character_set_client = @saved_cs_client;
 
---
--- Table structure for table `results`
---
+
+
+# Dump of table results
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `results`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+
 CREATE TABLE `results` (
   `cat_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `value` int(10) unsigned NOT NULL,
+  `value` decimal(11,2) NOT NULL,
   `result_date` datetime NOT NULL,
   KEY `cat_id` (`cat_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `results_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `categories` (`id`),
   CONSTRAINT `results_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-SET character_set_client = @saved_cs_client;
 
---
--- Table structure for table `users`
---
+
+
+# Dump of table users
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `users`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL auto_increment,
-  `username` char(255) default NULL,
-  `password` char(255) default NULL,
-  `email` char(255) default NULL,
-  PRIMARY KEY  (`id`),
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` char(255) DEFAULT NULL,
+  `password` char(255) DEFAULT NULL,
+  `email` char(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   UNIQUE KEY `login_unique` (`username`),
   UNIQUE KEY `email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
-SET character_set_client = @saved_cs_client;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2010-04-01  2:21:01
