725737023221b43d5dbccad3e9dd60575a4324ec
Artem Daniliants <artem@daniliants.com>
2010-04-28 12:47:30 +0300

    Fixed tiny bug. Forgot to add "

--- Server/application/views/api/results.php
+++ Server/application/views/api/results.php
@@ -1,5 +1,5 @@
 <?php echo "<?"; ?>xml version="1.0" encoding="utf-8" <?php echo "?>"; ?>
 
 <results>
-    <?php $i=1; foreach ($results as $r){ ?>   <result username="<?php echo $r->username; ?>" position="<?php echo $i; ?>" date="<?php echo $r->result_date; ?>" <?php if (isset($show_unit) AND $show_unit==false) echo 'unit='.$r->unit.'" '; ?>value="<?php echo $r->value; ?>" />
+    <?php $i=1; foreach ($results as $r){ ?>   <result username="<?php echo $r->username; ?>" position="<?php echo $i; ?>" date="<?php echo $r->result_date; ?>" <?php if (isset($show_unit) AND $show_unit==false) echo 'unit="'.$r->unit.'" '; ?>value="<?php echo $r->value; ?>" />
 <?php $i++; } ?></results>
