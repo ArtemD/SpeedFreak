c0ac3229cd181acc5e705e7473e6b9535c30cbe1
Artem Daniliants <artem@daniliants.com>
2010-05-25 14:59:39 +0300

    Fixed CDATA issue

--- Server/application/views/api/user_info.php
+++ Server/application/views/api/user_info.php
@@ -1,3 +1,3 @@
 <?php echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
 
-<user login="<?php echo $user->username ; ?>" description="<![CDATA[ <?php echo $user->description; ?> ]]>" last_activity="<?php echo $user->last_activity ; ?>" <?php if (isset($avatar)) echo 'avatar="'.$avatar.'" '; ?> />
+<user login="<?php echo $user->username ; ?>" description="<?php echo htmlentities($user->description); ?>" last_activity="<?php echo $user->last_activity ; ?>" <?php if (isset($avatar)) echo 'avatar="'.$avatar.'" '; ?> />
--- Server/application/views/api/user_list.php
+++ Server/application/views/api/user_list.php
@@ -2,7 +2,7 @@
 
 <users><?php if (count($list)>0) foreach($list as $user){ ?>
 	
-	<user login="<?php echo $user->username ; ?>" description="<![CDATA[ <?php echo $user->description; ?> ]]>" last_activity="<?php echo $user->last_activity ; ?>" <?php if (isset($avatar)) echo 'avatar="'.$avatar.'" '; ?> />
+	<user login="<?php echo $user->username ; ?>" description="<?php echo htmlentities($user->description); ?>" last_activity="<?php echo $user->last_activity ; ?>" <?php if (isset($avatar)) echo 'avatar="'.$avatar.'" '; ?> />
 	<?php } ?>
 
 </users>
