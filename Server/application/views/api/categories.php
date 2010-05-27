959e8704bff543f89534709099f7bcd9795e4cbe
Artem Daniliants <artem@daniliants.com>
2010-05-27 09:50:17 +0300

    Fixed XML parsing error

--- Server/application/views/api/categories.php
+++ Server/application/views/api/categories.php
@@ -1,4 +1,4 @@
-<?php echo "<?"; ?>xml version="1.0" encoding="utf-8" <?php echo "?>"; ?>
+<?php echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
 
 <categories>
     <?php foreach ($categories as $k){ ?>   <category description="<?php echo $k->description; ?>" unit="<?php echo $k->unit; ?>"><?php echo $k->slug; ?></category>
